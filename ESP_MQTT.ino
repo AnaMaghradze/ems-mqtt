5e6694777f5df94b5becebf001e177f2a2048c54
AnaMaghradze <70408579+AnaMaghradze@users.noreply.github.com>
2022-05-07 23:43:36 +0400

    update and clean up

--- ESP_MQTT.ino
+++ ESP_MQTT.ino
@@ -1,37 +1,44 @@
 #include "WiFi.h"
 #include "PubSubClient.h"
-#include <PZEM004Tv30.h>
-//#define ESP32
-/* Hardware Serial2 is only available on certain boards.
-   For example the Arduino MEGA 2560
-*/
+#include "PZEM004Tv30.h"
+
 #if defined(ESP32)
 PZEM004Tv30 pzem(Serial2, 13, 15);
 #else
 PZEM004Tv30 pzem(Serial2);
 #endif
-//#define MQTT_VERSION MQTT_VERSION_3_1
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
-const char* ssid = " ";   // Your_SSID 
-const char* password = " ";  // Your_Password 
+const char* ssid = "";      // SSID
+const char* password = "";  // Password
 
-const char *mqttServer = " "; // host ip
-const char *clientID = "101@doesnotmatter";
-const char *topic = "0";
+/*
+  clientID = "101@ESP32-1"; // ESP #1
+  clientID = "115@ESP32-2"; // ESP #2
+  clientID = "122@ESP32-3"; // ESP #3
+*/
+const char *mqttServer = ""; // Host IP
+const char *clientID = "101@ESP32-1";
 const uint16_t mqttPort = 3004;
-const char* mqttUser = " "; 
-const char* mqttPassword = " ";
+const char* mqttUser = "";
+const char* mqttPassword = "";
 
+// define topics for each value to send to the claud
+const char *voltage_topic     = "0";
+const char *current_topic     = "1";
+const char *power_topic       = "2";
+const char *energy_topic      = "3";
+const char *frequency_topic   = "4";
+const char *pf_topic          = "5";
 
 void setup()
 {
   // Set software serial baudrate to 115200;
   Serial.begin(115200);
 
-  // connecting to a WiFi network
+  // connecting to the WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -39,18 +46,14 @@ void setup()
   }
   Serial.println("");
   Serial.println("Connected to Wi-Fi");
-  // Serial.println(WiFi.localIP());
 
   // connecting to a mqtt broker
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
-  //  client.setKeepAlive (30);
-  //  client.setSocketTimeout (30);
 
   Serial.println("Connecting to MQTT Broker...");
   while (!client.connected()) {
     // Attempt to connect
-
     if (client.connect(clientID, mqttUser, mqttPassword)) {
       Serial.println("Sucessfully Connected to MQTT Broker.");
     }
@@ -63,18 +66,6 @@ void setup()
   }
 }
 
-
-void callback(char *topic, byte *payload, unsigned int length) {
-  Serial.print("New Message at topic: ");
-  Serial.println(topic);
-  Serial.print("Message: ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char) payload[i]);
-  }
-  Serial.println();
-  Serial.println("------------------------------------");
-}
-
 void loop() {
   if (!client.connected())
     reconnect();
@@ -83,7 +74,7 @@ void loop() {
   Serial.print("Custom Address:");
   Serial.println(pzem.readAddress(), HEX);
 
-  // Read the data from the sensor
+  // Read data from the sensors
   float voltage = pzem.voltage();
   float current = pzem.current();
   float power = pzem.power();
@@ -91,7 +82,7 @@ void loop() {
   float frequency = pzem.frequency();
   float pf = pzem.pf();
 
-  // Check if the data is valid
+  // Check data validity
   if (isnan(voltage)) {
     Serial.println("Error reading voltage");
   } else if (isnan(current)) {
@@ -105,24 +96,6 @@ void loop() {
   } else if (isnan(pf)) {
     Serial.println("Error reading power factor");
   } else {
-
-    // Print the values to the Serial console
-    Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
-    Serial.print("Current: ");      Serial.print(current, 5);      Serial.println("A");
-    Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
-    Serial.print("Energy: ");       Serial.print(energy, 3);     Serial.println("kWh");
-    Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
-    Serial.print("PF: ");           Serial.println(pf);
-
-    // define topics for each value to send to the claud
-    const char *voltage_topic = "0";
-    const char *amperage_topic = "1";
-    const char *power_topic = "2";
-    const char *energy_topic = "3";
-    const char *frequency_topic = "4";
-    const char *power_facrot_topic = "5";
-    
-    // convert float values into char[]
     char send_voltage[20];
     dtostrf(voltage, 2, 3, send_voltage);
     char send_current[20];
@@ -137,14 +110,22 @@ void loop() {
     dtostrf(pf, 2, 3, send_pf);
 
     // publish values to corresponding topics
-    client.publish("0", send_voltage);
-    client.publish("1", send_current);
-    client.publish("2", send_power);
-    client.publish("3", send_energy);
-    client.publish("4", send_frequency);
-    client.publish("5", send_pf);
-  }
+    client.publish(voltage_topic, send_voltage);
+    client.publish(current_topic, send_current);
+    client.publish(power_topic, send_power);
+    client.publish(energy_topic, send_energy);
+    client.publish(frequency_topic, send_frequency);
+    client.publish(pf_topic, send_pf);
 
+    // Print values to the Serial console
+    Serial.print("Voltage: ");    Serial.print(voltage);      Serial.println("V");
+    Serial.print("Current: ");    Serial.print(current, 5);   Serial.println("A");
+    Serial.print("Power: ");      Serial.print(power);        Serial.println("W");
+    Serial.print("Energy: ");     Serial.print(energy, 3);    Serial.println("kWh");
+    Serial.print("Frequency: ");  Serial.print(frequency, 1); Serial.println("Hz");
+    Serial.print("PF: ");         Serial.println(pf);
+
+  }
   Serial.println();
   delay(2000);
 }
@@ -164,4 +145,15 @@ void reconnect() {
       delay(2000);
     }
   }
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("New Message at topic: ");
+  Serial.println(topic);
+  Serial.print("Message: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char) payload[i]);
+  }
+  Serial.println();
+  Serial.println("------------------------------------");
 }
